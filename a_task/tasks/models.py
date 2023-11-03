6e107f8bfafe31265ea25a75fcedaa198fa2d5e3
Amaich <amaev773@mail.ru>
2023-11-04 01:31:57 +0300

    task model added

--- a_task/tasks/models.py
+++ a_task/tasks/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    summary = models.CharField(max_length=100, verbose_name='Краткое описание')
+    description = models.TextField(verbose_name='Описание')
+
