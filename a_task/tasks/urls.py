31e173260a39b066c51310c94da3d054e0a1870b
Amaich <amaev773@mail.ru>
2023-11-06 23:50:59 +0300

    auth started

--- a_task/tasks/urls.py
+++ a_task/tasks/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'tasks'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('new', views.TaskCreate.as_view(), name='task_create')
